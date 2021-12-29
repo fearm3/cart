06c18e92da463b91fcb331cdef54c61a2bbbdee6
fearm3 <ibraaa1628@gmail.com>
2021-12-29 14:17:08 +0300

    add Total and amount

--- src/reducer.js
+++ src/reducer.js
@@ -30,6 +30,23 @@ const reducer = (state, action) => {
       .filter((cartItem) => cartItem.amount !== 0);
     return { ...state, cart: tempCart };
   }
+  if (action.type === "GET_TOTALS") {
+    const { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    return { ...state, total, amount };
+  }
   return state;
 };
 
